4218f204d18a926a9512886254b938a1c976f7bc
Paola Garcia <pacri14@gmail.com>
2021-10-11 15:58:05 +0000

    Adds comments

--- main.rb
+++ main.rb
@@ -9,34 +9,38 @@ def start
   p1 = Player.new(1)
   p2 = Player.new(2)
  
+  #Instantiates new game with new players created above
   game = Game.new(p1, p2)
   
   # Starts game until there is a winner
   while game.winner == nil
- 
-
+  
+    #Requests a new question
     question = Game.new_question
     num1, num2, result = question 
+    current_player = game.current_player.number
 
-    puts "\nPlayer #{game.current_player.number}: What does #{num1} plus #{num2} equal? #{result}" 
+    puts "\nPlayer #{current_player}: What does #{num1} plus #{num2} equal? #{result}" 
     print "> " 
     response = gets.chomp.to_i
 
     if(response == result)
-      puts "Player #{game.current_player.number}: Way to go!"
+      puts "Player #{current_player}: Way to go!"
     end
 
     if (response != result)
-      puts "Player #{game.current_player.number}: Seriously? No!"
+      puts "Player #{current_player}: Seriously? No!"
       game.current_player.decrease_lives
+      #check the latest score to verify if there is a winner
       game.check_winner 
     end
-
-    game.switch_player   
-    puts game.score
+ 
 
     if (game.winner == nil)
       puts "----- NEW TURN -----"
+    elsif 
+      game.switch_player   
+      puts game.score
     end
   end
 
@@ -44,7 +48,7 @@ def start
   puts "Player #{game.winner.number} wins with a score of #{game.winner.lives}/3 "
   puts "----- GAME OVER -----"
   puts "Good bye!"
-     
+
 end
 
 start
